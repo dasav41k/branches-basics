3961b94e337b9f50dad4deb43f961c5f041ba3a0
dasav41k <115127112+dasav41k@users.noreply.github.com>
2023-05-02 13:10:21 +0700

    Add files via upload

---
+++ app.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("How are you?");
+    return 0;
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!");
+    printf("I'm Bob");
+    printf("I'm 17 y.o.");
+    return 0;
+}
